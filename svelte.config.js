54c13b48fee0b6e7e076023f4106628f630b842e
Bonifasius Khrisna <bonifasiuskhris@live.com>
2022-12-07 19:23:28 +0700

    sveltekit install

---
+++ svelte.config.js
@@ -0,0 +1,23 @@
+// import adapter from '@sveltejs/adapter-auto';
+import adapter from "@sveltejs/adapter-static"; 
+
+const dev = "production" === "development";
+// const dev = process.env.NODE_ENV === 'development';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: adapter({
+			pages: "docs",
+			assets: "docs"
+		}),
+		paths: {
+			// change below to your repo name
+			base: dev ? '' : '/bonifasiuskhris.github.io',
+		},
+		// hydrate the <div id="svelte"> element in src/app.html
+		// target: "#svelte"
+	}
+};
+
+export default config;

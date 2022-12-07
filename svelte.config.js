807f854ccfb0094fc031fd0fdc2ceb90623502e2
Bonifasius Khrisna <bonifasiuskhris@live.com>
2022-12-07 20:48:26 +0700

    new update layout and template

--- svelte.config.js
+++ svelte.config.js
@@ -1,24 +1,30 @@
 // import adapter from '@sveltejs/adapter-auto';
-import adapter from "@sveltejs/adapter-static"; 
+import adapter from '@sveltejs/adapter-static';
+import preprocess from 'svelte-preprocess';
 
-const dev = "production" === "development";
 // const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter({
-			pages: "docs",
-			assets: "docs"
+			pages: 'docs',
+			assets: 'docs'
 		}),
-		paths: {
-			// change below to your repo name
-			// base: dev ? '' : '/bonifasiuskhris.github.io',
-			base: '',
-		},
+		// paths: {
+		// 	// change below to your repo name
+		// 	// base: dev ? '' : '/bonifasiuskhris.github.io',
+		// 	base: '',
+		// },
 		// hydrate the <div id="svelte"> element in src/app.html
 		// target: "#svelte"
-	}
+	},
+
+	preprocess: [
+		preprocess({
+			postcss: true
+		})
+	]
 };
 
 export default config;

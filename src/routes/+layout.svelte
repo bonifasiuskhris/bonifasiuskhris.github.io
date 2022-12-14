705f0a0dc60d982248f55b315c0787ef04b3db9e
Bonifasius Khrisna <bonifasiuskhris@live.com>
2022-12-14 17:33:55 +0700

    new update page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,13 +2,13 @@
 	import '../app.postcss';
 </script>
 
-<div class="navbar bg-primary text-primary-content">
-	<div class="container mx-auto">
-		<a class="text-xl">bonifasiuskhris</a>
+<!-- <div class="navbar bg-primary text-primary-content">
+	<div class="w-full md:w-1/2 mx-auto">
+		<a href="javascript;" class="text-xl">bonifasiuskhris</a>
 	</div>
-</div>
+</div> -->
 
-<div class="container mx-auto">
+<div class="md:w-1/2 md:mx-auto">
 	<div class="my-6">
 		<slot />
 	</div>
